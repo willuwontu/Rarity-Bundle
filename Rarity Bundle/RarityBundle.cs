9eb58305e9b40f77b8a063c57c8eb29e1521bfc1
willuwontu <willuwontu3@gmail.com>
2023-01-01 17:01:29 -0800

    Update RarityBundle.cs

--- Rarity Bundle/RarityBundle.cs
+++ Rarity Bundle/RarityBundle.cs
@@ -29,22 +29,4 @@ namespace RarityBundle
             RarityLib.Utils.RarityUtils.AddRarity("Divine", 0.0015625f, new Color32(255, 255, 180, 255), new Color32(125, 125, 100, 255));
         }
     }
-
-    public static class Rarities
-    {
-        public static CardInfo.Rarity GetRarity(string name)
-        {
-            return RarityLib.Utils.RarityUtils.GetRarity(name);
-        }
-        public static CardInfo.Rarity Trinket => RarityLib.Utils.RarityUtils.GetRarity("Trinket");
-        public static CardInfo.Rarity Common => CardInfo.Rarity.Common;
-        public static CardInfo.Rarity Scarce => RarityLib.Utils.RarityUtils.GetRarity("Scarce");
-        public static CardInfo.Rarity Uncommon => CardInfo.Rarity.Uncommon;
-        public static CardInfo.Rarity Exotic => RarityLib.Utils.RarityUtils.GetRarity("Exotic");
-        public static CardInfo.Rarity Rare => CardInfo.Rarity.Rare;
-        public static CardInfo.Rarity Epic => RarityLib.Utils.RarityUtils.GetRarity("Epic");
-        public static CardInfo.Rarity Legendary => RarityLib.Utils.RarityUtils.GetRarity("Legendary");
-        public static CardInfo.Rarity Mythical => RarityLib.Utils.RarityUtils.GetRarity("Mythical");
-        public static CardInfo.Rarity Divine => RarityLib.Utils.RarityUtils.GetRarity("Divine");
-    }
 }
