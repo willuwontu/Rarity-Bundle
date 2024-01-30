235be700bef9919fbf169414457724d60997adcd
System-of-Root <root@r00t.systems>
2024-01-30 04:21:26 -0500

    Trinket rebalance

--- Rarity Bundle/RarityBundle.cs
+++ Rarity Bundle/RarityBundle.cs
@@ -1,4 +1,6 @@
 ﻿using BepInEx;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace RarityBundle
@@ -11,13 +13,14 @@ namespace RarityBundle
     {
         private const string ModId = "com.CrazyCoders.Rounds.RarityBundle";
         private const string ModName = "RarityBundle";
-        public const string Version = "0.0.0"; // What version are we on (major.minor.patch)?
+        public const string Version = "0.1.0"; // What version are we on (major.minor.patch)?
 
         public static RarityBundle instance { get; private set; }
 
         void Awake()
         {
-            RarityLib.Utils.RarityUtils.AddRarity("Trinket", 3, new Color(0.38f, 0.38f, 0.38f), new Color(0.0978f, 0.1088f, 0.1321f));
+            var plugins = (List<BaseUnityPlugin>)typeof(BepInEx.Bootstrap.Chainloader).GetField("_plugins", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+            RarityLib.Utils.RarityUtils.AddRarity("Trinket", plugins.Exists(plugin => plugin.Info.Metadata.GUID == "Root.Rarity.Dard.Draw") ? 1.75f:3f, new Color(0.38f, 0.38f, 0.38f), new Color(0.0978f, 0.1088f, 0.1321f));
             // Common 1
             RarityLib.Utils.RarityUtils.AddRarity("Scarce", 0.7f, new Color32(0, 172, 98, 255), new Color32(0, 95, 60, 255));
             // Uncommon 0.4
